cd3f55d1c9e2ebaa484fc8b9ea16409cf9daa748
Ellis Guillen <710619@student.dusd.net>
2024-11-08 15:29:29 -0800

    Added scripts and messed with the lighting.

---
+++ ProjectEpsilonP7/Assets/Scripts/Scripts from Ender's other projects/ActivateGameOver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGameOver : MonoBehaviour
+{
+    public SpriteRenderer visibility;
+    public GameObject gameOverScreen;
+    void Start()
+    {
+        visibility = GetComponent<SpriteRenderer>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        gameOverScreen.SetActive(true);
+        visibility.gameObject.SetActive(false);
+    }
+}
---
+++ ProjectEpsilonP7/Assets/Scripts/Scripts from Ender's other projects/PlayerControllerOther.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerOther : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float canWeGetMuchHigher;
+    private float horizontal;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, canWeGetMuchHigher));
+        }
+        transform.Translate (new Vector2(horizontal * speed * Time.deltaTime, 0f));
+    }
+}
