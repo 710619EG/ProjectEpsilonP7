c133f503a65bcffbd400625b3bb506f67dbc1fd6
718355 <718355@student.dusd.net>
2024-11-30 13:56:52 -0800

    Tried to fix some code.

--- ProjectEpsilonP7/Assets/Scripts/CameraFollow.cs
+++ ProjectEpsilonP7/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    GameObject Epsilon;
     // Start is called before the first frame update
     void Start()
+    
     {
-        
+        Epsilon = GameObject.Find("Epsilon");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(Epsilon.transform.position.x, Epsilon.transform.position.y + 5, Epsilon.transform.position.z - 10);
     }
 }
