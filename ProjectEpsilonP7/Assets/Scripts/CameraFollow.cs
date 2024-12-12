c28105d1a08f59c5b07d989aef7e4e55da250b1a
Ellis Guillen <116373777+710619EG@users.noreply.github.com>
2024-12-12 11:49:27 -0800

    edited CameraFollow

--- ProjectEpsilonP7/Assets/Scripts/CameraFollow.cs
+++ ProjectEpsilonP7/Assets/Scripts/CameraFollow.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform Target;
     // Start is called before the first frame update
     void Start()
-    
     {
-        target.GetComponent <Transform>();
+        //Here the script gets the transform of the target(CameraTarget)
+        Target.GetComponent <Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.positon = target.position;
+        transform.positon = Target.position;
     }
 }
