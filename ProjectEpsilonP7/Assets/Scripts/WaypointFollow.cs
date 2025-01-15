12e520740318646337182f3857f1447d7d4aa9f0
Ellis G. <710619@student.dusd.net>
2025-01-15 09:55:03 -0800

    Started the AI's waypoint follow script

---
+++ ProjectEpsilonP7/Assets/Scripts/WaypointFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollow : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int currentNode = 0;
+
+    public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+  
+    }
+}
